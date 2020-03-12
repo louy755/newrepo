d5c01ce19892dd677b16d7c0b96ce4e93703d9dd
louy755 <lumberjack1695@yahoo.com>
2020-03-12 02:08:27 -0500

    new

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ module Mngdr2
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
+    Rails.autoloaders.main.ignore("#{Rails.root}/app/overrides")
     config.time_zone = 'Eastern Time (US & Canada)'
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
